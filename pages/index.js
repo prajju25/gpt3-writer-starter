ba375fd32f17e1959c74ab8c1794b50315d8f3c1
prajju25 <kotian.prajwal821@gmail.com>
2023-01-12 02:09:56 +0530

    latest build

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,33 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import { useState } from 'react';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('');
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,11 +36,42 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Birth Name Recommender</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>
+              Provide gender, nationality, starting character 
+              or ending character and acronymn as input to recommend number of names of particular origin
+            </h2>
           </div>
+        </div>
+        {/* Add this code here*/}
+        <div className="prompt-container">
+          <textarea placeholder="start typing here" className="prompt-box"
+          value={userInput}
+          onChange={e=>setUserInput(e.target.value)} />
+          <div className="prompt-buttons">
+            <a
+              className={isGenerating ? 'generate-button loading' : 'generate-button'}
+              onClick={callGenerateEndpoint}
+            >
+              <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+              </div>
+            </a>
+          </div>
+          {apiOutput && (
+            <div className="output">
+              <div className="output-header-container">
+                <div className="output-header">
+                  <h3>Output</h3>
+                </div>
+              </div>
+              <div className="output-content">
+                <p>{apiOutput}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
       <div className="badge-container grow">
